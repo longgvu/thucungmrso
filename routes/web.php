77fd0775eff76fb48e3ce24d9fb886da6d1752b5
lqthien <quocthien0404@gmail.com>
2023-12-25 10:37:11 +0700

    install admin

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,8 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\PageController;
-use App\Http\Controllers\IntroduceController;
-use App\Http\Controllers\CartController;
-use App\Http\Controllers\AuthController;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -16,21 +14,18 @@ use App\Http\Controllers\AuthController;
 |
 */
 
-
-Route::get('/', [PageController::class, 'index']);
-Route::get('/index', function () {
-    return view('index');
-});
-Route::get('introduce', [IntroduceController::class, 'home']);
-Route::get('service', [IntroduceController::class, 'home']);
-Route::get('blog', [IntroduceController::class, 'home']);
-Route::get('recruitment', [IntroduceController::class, 'home']);
-Route::get('contact', [IntroduceController::class, 'home']);
-Route::middleware(['auth'])->group(function () { 
-    // Các routes mà người dùng cần phải đăng nhập để truy cập
-    Route::post('/cart/add/{product}', [CartController::class, 'addToCart'])->name('cart.add');
-    
+Route::get('/', function () {
+    return view('welcome');
 });
 
-Route::get('/login', [AuthController::class, 'showLoginForm'])->name('login');
-Route::post('/login', [AuthController::class, 'login']);
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
